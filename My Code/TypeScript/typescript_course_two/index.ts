2d5970e83ca7224097c1df01a7afcdb3cbd6d7f7
Mbugua Caleb <mbuguacaleb30@gmail.com>
2021-12-06 15:11:22 +0300

    beginning-with-simple-types

---
+++ My Code/TypeScript/typescript_course_two/index.ts
@@ -0,0 +1,34 @@
+//Number Type
+let age = 24
+
+let weight: number = 170
+
+weight = 180
+
+weight = 160
+
+//Type scipt is able to inform us of errors even before we run
+//our code
+
+//weight = '160'
+
+age = 25
+age = 23
+
+//Typescript also infers type
+age = 'one'
+
+//Type String
+
+let myName = 'Mbugua Caleb'
+let myDogName: string = 'Hash'
+
+myName = true
+
+myName = 'MbuguaCaleb'.length
+myName = 'MbuguaCaleb'.length === 0
+
+//type Boolean
+let isAwesome = false
+let isAwesome2 = 'MbuguaCaleb'.length === 0
+let isBoring: boolean = false
---
+++ My Code/TypeScript/typescript_course_two/readme.md
@@ -0,0 +1,17 @@
+**What is Type Script**
+
+```
+It takes in Variables and assigns types to the variables.
+
+```
+
+**Inference**
+
+```
+It is a powerful feature from typescript where typeScript is able to automatically
+assign a type to my variable incase i had not assigned it manually
+
+TypeScript is very Powerful since it is able to learn and predict the results of
+certain types before hand.
+
+```
