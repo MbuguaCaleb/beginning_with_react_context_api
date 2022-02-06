99e3f112eafc0d7f06c49fed0dceeb1e41a317b9
Mbugua Caleb <mbuguacaleb30@gmail.com>
2022-02-06 17:37:29 +0300

    material-ui-grid-layoout

---
+++ My Code/React/react_material_ui_lesson_one/src/components/TourCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Paper from '@mui/material/Paper'
+import Grid from '@mui/material/Grid'
+
+const TourCard = () => {
+  return (
+    <Grid item xs={3}>
+      <Paper elevation={3} variant='outlined'>
+        Hello
+      </Paper>
+    </Grid>
+  )
+}
+
+export default TourCard
